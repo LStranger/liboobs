8a752c514cba0371d6d0c33c061ffb091feb62a2
Carlos Garnacho <carlosg@src.gnome.org>
2006-09-15 17:29:41 +0000

    Argh, add these files, really.

---
+++ src/oobs-groupsconfig-private.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C; c-file-style: "gnu"; tab-width: 8 -*- */
+/* Copyright (C) 2005 Carlos Garnacho
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Authors: Carlos Garnacho Parro  <carlosg@gnome.org>
+ */
+
+#ifndef __OOBS_GROUPSCONFIG_PRIVATE_H
+#define __OOBS_GROUPSCONFIG_PRIVATE_H
+
+G_BEGIN_DECLS
+
+#include "oobs-groupsconfig.h"
+
+guint       _oobs_groups_config_get_id (OobsGroupsConfig *config);
+
+G_END_DECLS
+
+#endif /* __OOBS_GROUPSCONFIG_PRIVATE_H */
---
+++ src/oobs-usersconfig-private.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C; c-file-style: "gnu"; tab-width: 8 -*- */
+/* Copyright (C) 2005 Carlos Garnacho
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Authors: Carlos Garnacho Parro  <carlosg@gnome.org>
+ */
+
+#ifndef __OOBS_USERSCONFIG_PRIVATE_H
+#define __OOBS_USERSCONFIG_PRIVATE_H
+
+G_BEGIN_DECLS
+
+#include "oobs-usersconfig.h"
+
+guint       _oobs_users_config_get_id (OobsUsersConfig *config);
+
+G_END_DECLS
+
+#endif /* __OOBS_USERSCONFIG_PRIVATE_H */
